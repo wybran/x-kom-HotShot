a4bface85686d239fa6dc7c43cf732b44adccf04
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-12-29 22:04:27 +0100

    chore: Change embed url to promo-dedicated url

--- utils/embed.js
+++ utils/embed.js
@@ -2,7 +2,7 @@ export const embed = (data) => {
     return {
         title: data.PromotionName,
         description: `~~${data.OldPrice}~~ **${data.Price}** zł\n${data.PromotionGainText}`,
-        url: `https://www.x-kom.pl`,
+        url: `https://www.x-kom.pl/goracy_strzal`,
         thumbnail: {
             url: data.PromotionPhoto.ThumbnailUrl
         }
