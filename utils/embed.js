7159a844f15610167fccb314bdc81c2478332c52
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-12-29 22:44:11 +0100

    feat: ✨ Make embed look cooler

--- utils/embed.js
+++ utils/embed.js
@@ -1,10 +1,26 @@
 export const embed = (data) => {
     return {
-        title: data.PromotionName,
-        description: `~~${data.OldPrice}~~ **${data.Price}** zł\n${data.PromotionGainText}`,
+        title: "Gorrrący strzał! 🔥🥵",
+        description: `Zdobądź **${data.PromotionName}** w obniżonej cenie - tylko przez najbliższe 12 godzin (lub do wyczerpania zapasów).`,
         url: `https://www.x-kom.pl/goracy_strzal`,
-        thumbnail: {
+        color: 1037054,
+        footer: {
+            icon_url: "https://cdn.discordapp.com/avatars/1057682359383765062/8d8f7bd366698e702a47584e46b1e54c.png",
+            text: "Gorący Strzał x-kom by wybran"
+        },
+        image: {
             url: data.PromotionPhoto.ThumbnailUrl
-        }
+        },
+        author: {
+            name: "x-kom.pl",
+            url: "https://x-kom.pl",
+            icon_url: "https://cdn.discordapp.com/avatars/1057682359383765062/8d8f7bd366698e702a47584e46b1e54c.png"
+        },
+        fields: [
+            {
+                name: data.PromotionGainText,
+                value: `~~${data.OldPrice}~~ **${data.Price}** zł`
+            }
+        ]
     };
 };
